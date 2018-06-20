6202a8e14ab4b594f74cf0cfd42bf632656d3a00
mahmou6 <mahmou6@gmail.com>
2018-06-20 18:33:52 +0200

    View

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,45 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Mahmoud on 6/20/18.
+//  Copyright © 2018 Mahmoud. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
